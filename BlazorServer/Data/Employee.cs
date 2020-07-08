6599ca5088336cf8376a895513828b70d437e9bc
Supreet <suprith.prakash.sp@gmail.com>
2020-07-09 00:40:12 +0530

    Created Employee Component,EMployee Model and View

---
+++ BlazorServer/Data/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Data
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+    }
+}
---
+++ BlazorServer/Pages/EmployeeListBase.cs
@@ -0,0 +1,35 @@
+﻿using BlazorServer.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+        protected override Task OnInitializedAsync()
+        {
+            GetEmployee();
+            return base.OnInitializedAsync();
+        }
+        private void GetEmployee()
+        {
+            var employee1 = new Employee
+            {
+                id = 1,
+                Name = "Tom",
+                Designation = "TechLead"
+            };
+            var employee2 = new Employee
+            {
+                id = 2,
+                Name = "Harry",
+                Designation = "Software Engineer"
+            };
+            Employees = new List<Employee> { employee1, employee2 };
+        }
+    }
+}
