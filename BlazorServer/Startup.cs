2e0bdbcb6983df4b2d0ec090768761ab97f34e86
Supreet <suprith.prakash.sp@gmail.com>
2020-07-15 17:37:00 +0530

    HttpPost implementation using webapi and mongodb

--- BlazorServer/Startup.cs
+++ BlazorServer/Startup.cs
@@ -42,9 +42,11 @@ namespace BlazorServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+        
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                
             }
             else
             {
