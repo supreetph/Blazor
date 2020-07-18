3aa3ce16aa368daf773251767ee0c48bb0619c0e
Supreet <suprith.prakash.sp@gmail.com>
2020-07-18 14:52:49 +0530

    aws sqs integration

---
+++ AWSService/AWSSQSHelper.cs
@@ -0,0 +1,50 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWSService
+{
+   public class AWSSQSHelper
+    {
+        private static AmazonSQSClient Client
+        {
+            get
+            {
+                var Client = new AmazonSQSClient("", "", RegionEndpoint.USWest2);
+                return Client;
+            }
+        }
+
+
+        public void SendMessage(string url, Message sqsMessage)
+        {
+            try
+            {
+                var sendMessageRequest = new SendMessageRequest
+                {
+                    QueueUrl = url,
+                    MessageBody = JsonConvert.SerializeObject(sqsMessage.Body),
+                   // MessageAttributes = attributes.ToDictionary(item => item.Key, item => (MessageAttributeValue)item.Value),
+
+
+                };
+                var sqsSendMessage = Client.SendMessageAsync(sendMessageRequest).Result;
+            }
+
+            catch (AmazonSQSException ex)
+            {
+                throw new Exception(ex.Message);
+
+
+            }
+        }
+
+
+    }
+}
---
+++ AWSService/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AWSService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+ 
+
+        }
+    }
+}
